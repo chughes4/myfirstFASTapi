b9ccf926fd02d790b41ac49caa202398284b83c6
chughes4 <chughes4life@gmail.com>
2023-01-18 23:29:01 -0500

    added !

--- app/main.py
+++ app/main.py
@@ -30,7 +30,7 @@ app.include_router(vote.router)
 
 @app.get("/")
 def root():
-    return {"message": "Joy to the World",
+    return {"message": "Joy to the World!",
             "gimmie": "A dollar"
           }
  
